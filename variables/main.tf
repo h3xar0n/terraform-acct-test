71916c117c5eeadbf2b26fbc6358ad614c6fd4eb
h3xar0n <aron@eideldev.com>
2020-06-19 12:34:27 -0700

    use map

--- variables/main.tf
+++ variables/main.tf
@@ -30,6 +30,11 @@ variable "mymap" {
   }
 }
 
+variable "input" {
+  type        = string
+  description = "Set the name of the VPC"
+}
+
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
