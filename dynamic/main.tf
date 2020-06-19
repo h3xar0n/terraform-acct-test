90a2cbc5281539e54b2a605e73cac14da7ae1c59
h3xar0n <aron@eideldev.com>
2020-06-19 12:58:00 -0700

    add security group

--- dynamic/main.tf
+++ dynamic/main.tf
@@ -21,3 +21,29 @@ resource "aws_instance" "myec2db" {
     Name = "Web Server"
   }
 }
+
+resource "aws_security_group" "web_traffic" {
+  name = "Secure Server"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress_rules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    iterator = port
+    for_each = var.egress_rules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
