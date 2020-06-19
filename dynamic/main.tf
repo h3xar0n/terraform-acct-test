b11b6b916be322b2be41ebb061885013e3c74d88
h3xar0n <aron@eideldev.com>
2020-06-19 12:50:33 -0700

    add ec2 with sg outbound rules

--- dynamic/main.tf
+++ dynamic/main.tf
@@ -8,6 +8,11 @@ variable "ingress_rules" {
   default = []
 }
 
+variable "egress_rules" {
+  type    = list(number)
+  default = []
+}
+
 resource "aws_instance" "myec2db" {
   ami           = "ami-01a6e31ac994bbc09"
   instance_type = "t2.micro"
